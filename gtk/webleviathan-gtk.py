26b959400c6d4bcb0291e3fd2b4f4337d6b0e229
Scott Zeid <s@srwz.us>
2011-06-13 17:55:39 -0500

    Added main function to webleviathan-gtk and enter button works in set URL box

--- gtk/webleviathan-gtk.py
+++ gtk/webleviathan-gtk.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python
 
+# Leviathan Music Player
+# A Web-based music player based on the Leviathan music library manager.
+# 
+# Copyright (C) 2010-2011 Scott Zeid
+# http://me.srwz.us/leviathan/web
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
+# Except as contained in this notice, the name(s) of the above copyright holders
+# shall not be used in advertising or otherwise to promote the sale, use or
+# other dealings in this Software without prior written authorization.
+
 APP_NAME    = "Leviathan Music Player"
 APP_ID      = "webleviathan-gtk"
 APP_VERSION = "1.0"
@@ -67,6 +95,42 @@ def get_url():
  c = gconf.Client()
  return c.get_string(APP_URL_KEY)
 
+def main(argv):
+ url = get_url()
+ if not url or not check_url(url):
+  run_set_url_dialog()
+  url = get_url()
+  if not url:
+   return 1
+ elif "-s" in argv or "--set-url" in argv:
+  run_set_url_dialog()
+  return 0
+ w = gtk.Window(gtk.WINDOW_TOPLEVEL)
+ w.set_title(APP_NAME)
+ w.set_role("mainWindow")
+ set_icon(w)
+ w.set_size_request(1, 1)
+ w.resize(800, 500)
+ w.connect("delete-event", close_callback)
+ s = gtk.ScrolledWindow()
+ wv = webkit.WebView()
+ wv.open(url)
+ s.add(wv)
+ w.add(s)
+ try:
+  import gnome
+  import gnome.ui
+  gnome.init(APP_ID, APP_VERSION)
+  client = gnome.ui.master_client()
+  client.connect("die", gtk.main_quit)
+ except ImportError:
+  pass
+ while gtk.events_pending():
+  gtk.main_iteration()
+ w.show_all() 
+ gtk.main()
+ return 0
+
 def run_set_url_dialog():
  SP = 5
  url = get_url() or ""
@@ -82,6 +146,7 @@ def run_set_url_dialog():
  gadd(gtk.Label("Please enter the URL for your copy of the"), vbox)
  gadd(gtk.Label(APP_NAME + "."), vbox)
  url_box = gadd(gtk.Entry(), vbox, padding=SP)
+ url_box.set_activates_default(True)
  url_box.set_text(url)
  def cancel_callback(btn=None):
   dialog.hide()
@@ -102,6 +167,7 @@ def run_set_url_dialog():
  dialog.action_area.get_children()[0].connect("clicked", ok_callback)
  dialog.action_area.get_children()[1].connect("clicked", cancel_callback)
  dialog.connect("delete-event", dlg_close_callback)
+ dialog.set_default(dialog.action_area.get_children()[0])
  dialog.show()
  while dialog.get_visible():
   gtk.main_iteration(False)
@@ -131,44 +197,6 @@ def set_url(url):
  c.set_string(APP_URL_KEY, url)
 
 try:
- url = get_url()
- if not url or not check_url(url):
-  run_set_url_dialog()
-  url = get_url()
-  if not url:
-   sys.exit(1)
- elif "-s" in sys.argv or "--set-url" in sys.argv:
-  run_set_url_dialog()
-  sys.exit(0)
- 
- w = gtk.Window(gtk.WINDOW_TOPLEVEL)
- w.set_title(APP_NAME)
- w.set_role("mainWindow")
- set_icon(w)
- w.set_size_request(1, 1)
- w.resize(800, 500)
- w.connect("delete-event", close_callback)
- 
- s = gtk.ScrolledWindow()
- wv = webkit.WebView()
- wv.open(url)
- s.add(wv)
- w.add(s)
- 
- try:
-  import gnome
-  import gnome.ui
-  gnome.init(APP_ID, APP_VERSION)
-  client = gnome.ui.master_client()
-  client.connect("die", gtk.main_quit)
- except ImportError:
-  pass
- 
- while gtk.events_pending():
-  gtk.main_iteration()
- 
- w.show_all() 
- gtk.main()
+ sys.exit(main(sys.argv))
 except KeyboardInterrupt:
- print "bnay"
- sys.exit()
+ pass
